ce22cd2e6872f045a2072a9ebd26dcfde8a28fdb
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-08-14 09:45:13 +0500

    structuring start

---
+++ Full stack Assigement/src/components/hero/Hero.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+
+      {/* Left Content */}
+      <div className="hero-content">
+
+        <h1>
+          FIND CLOTHES
+          <br />
+          THAT MATCHES
+          <br />
+          YOUR STYLE
+        </h1>
+
+        <p className="hero-description">
+          Browse through our diverse range of meticulously crafted garments,
+          designed to bring out your individuality and cater to your sense of style.
+        </p>
